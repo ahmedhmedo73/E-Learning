f9f24a943456dfda6148582731e5bcc97ee2bba1
ahmed kamel <58473744+ahmedhmedo73@users.noreply.github.com>
2022-07-29 13:50:45 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/pages/auth/login/login.component';
+import { RegisterComponent } from './view/pages/auth/register/register.component';
+import { HomeComponent } from './view/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/view/pages/auth/login/login.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+// import { StatusService } from 'src/app/services/status.service';
+// import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-login-in',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  // VARIABLES
+  sign_in_form!: FormGroup;
+  alert_message: string = '';
+  alert_message_status: boolean = false;
+
+  // FUNCTIONS
+  constructor(
+    // private statusS: StatusService,
+    private fb: FormBuilder,
+    // private userS: UserService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // SET HEADER AND FOOTER DISPLAY
+    // this.statusS.set_header_footer_status(false);
+
+    // CONFIGURE THE FORM
+    this.sign_in_form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    // AUTO-LOGGING WHEN SIGNED IN
+    // if (this.userS.get_signed_in()) {
+    //   this.router.navigate(['/home']);
+    // }
+  }
+
+  sign_in() {
+    console.log('signed in!');
+    console.log(this.sign_in_form);
+
+    // SIGN IN
+    // let sign_in_status = this.userS.sign_in(this.sign_in_form.value);
+    let sign_in_status = '200';
+
+    if (sign_in_status == '200') {
+      this.router.navigate(['/home']);
+    } else {
+      // SHOW ALERT MESSAGE
+      this.alert_message = sign_in_status;
+      this.alert_message_status = true;
+
+      setTimeout(() => {
+        this.alert_message_status = false;
+      }, 2000);
+
+      // RESET FORM
+      this.sign_in_form.reset();
+    }
+  }
+}

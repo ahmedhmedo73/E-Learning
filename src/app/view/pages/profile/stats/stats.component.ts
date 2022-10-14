551a9f2e3dbece3d718558732ff85ec2d481b0ff
ahmed kamel <58473744+ahmedhmedo73@users.noreply.github.com>
2022-10-14 10:14:53 +0200

    stats page

--- src/app/view/pages/profile/stats/stats.component.ts
+++ src/app/view/pages/profile/stats/stats.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.scss']
+  styleUrls: ['./stats.component.scss'],
 })
 export class StatsComponent implements OnInit {
+  stats: any = [
+    {
+      date: '19/07/2022',
+      total: 25,
+      success: 11,
+      fail: 9,
+    },
+    {
+      date: '19/07/2022',
+      total: 25,
+      success: 11,
+      fail: 9,
+    },
+    {
+      date: '19/07/2022',
+      total: 25,
+      success: 11,
+      fail: 9,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
